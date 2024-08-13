71cdc0bf7e563492ded149c8b76f63d2d57dff64
seanlongcc <seanlongcc@gmail.com>
2024-08-13 10:10:01 -0400

    responsive font size

--- seanlongcc-site/src/pages/Home.tsx
+++ seanlongcc-site/src/pages/Home.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
     <div className='flex flex-col justify-center h-screen text-center'>
-      <span className='text-6xl font-thin '>annyeonghaseyo, sean imnida</span>
+      <span className='text-3xl font-thin lg:text-8xl '>
+        annyeonghaseyo, sean imnida
+      </span>
     </div>
   );
 };
