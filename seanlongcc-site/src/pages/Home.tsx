4e2323b71ef69dccba440669ed030d9ebe12447b
seanlongcc <seanlongcc@gmail.com>
2024-08-13 10:06:54 -0400

    update font and theme

--- seanlongcc-site/src/pages/Home.tsx
+++ seanlongcc-site/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
-    <div className='flex flex-col text-center justify-center h-screen'>
-      <span className='text-6xl font'>annyeonghaseyo, sean imnida</span>
+    <div className='flex flex-col justify-center h-screen text-center'>
+      <span className='text-6xl font-thin '>annyeonghaseyo, sean imnida</span>
     </div>
   );
 };
