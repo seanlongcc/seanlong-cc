d775017aab115542039a398089252674427d2bef
seanlongcc <seanlongcc@gmail.com>
2024-08-13 10:31:07 -0400

    update font

--- seanlongcc-site/src/pages/Home.tsx
+++ seanlongcc-site/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
-    <div className='flex flex-col justify-center h-screen text-center'>
-      <span className='text-3xl font-thin lg:text-8xl '>
+    <div className='flex flex-col justify-center h-screen not-italic text-center'>
+      <span className='text-3xl font-thin transition-all hover:font-black lg:text-8xl'>
         annyeonghaseyo, sean imnida
       </span>
     </div>
